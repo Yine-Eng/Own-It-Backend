fc9d7fc78feae95b086fd2335b2655fd613e71ea
Yine-Eng <sampsonanaba4@gmail.com>
2024-09-16 02:21:03 -0400

    Updated vercel.json for proper API routing

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ require('dotenv').config({ path: './backend/.env' });
 // CORS middleware with specified allowed origin
 const corsOptions = {
     origin: 'https://own-it-rental.vercel.app',
-    optionsSuccessStatus: 200,
+    methods: 'GET, POST', // Specify allowed methods
+    optionsSuccessStatus: 200, // For legacy browsers support
 };
 app.use(cors(corsOptions));
 
